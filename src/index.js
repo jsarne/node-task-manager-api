fe32c53ccc122692812095e8e85b1123f1a658c9
James Sarne <jim.sarne@gmail.com>
2020-05-14 16:30:13 -0700

    added support for HOST env var

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const app = require('./app');
 
-app.listen(process.env.PORT, () => {
-  console.log('Server is up on port ' + process.env.PORT);
-});
+// to run on 127.0.0.1 instead of localhost (useful for hitting this service from a device emulator)
+if (process.env.HOST) {
+  app.listen(process.env.PORT, process.env.HOST, () => {
+    console.log(`Server is up at ${process.env.HOST}:${process.env.PORT}`);
+  });  
+} else {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is up on port ${process.env.PORT}`);
+  });
+}
